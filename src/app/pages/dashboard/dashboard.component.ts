042f995ee7cf4a9ef80d7ae49d05bd55aa66cc64
k-mta <kenn.mta@gmail.com>
2021-06-18 22:48:56 +0700

    dashboard social

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AppConfig } from 'src/app/showcase/domain/appconfig';
+import { AppConfigService } from 'src/app/showcase/service/appconfigservice';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+})
+export class DashboardComponent implements OnInit {
+  config: AppConfig;
+  rightSide: boolean;
+
+  subscription: Subscription;
+
+  constructor(private configService: AppConfigService) {}
+
+  ngOnInit() {
+      this.config = this.configService.config;
+      this.subscription = this.configService.configUpdate$.subscribe(config => this.config = config);
+  }
+
+  ngOnDestroy() {
+      if (this.subscription) {
+          this.subscription.unsubscribe();
+      }
+  }
+}
