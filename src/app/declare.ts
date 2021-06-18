722bb3b6b645b1fc395f60829d8d4155290bd4ce
k-mta <kenn.mta@gmail.com>
2021-06-18 23:10:02 +0700

    sửa lỗi server định nghĩa sai urlpath

--- src/app/declare.ts
+++ src/app/declare.ts
@@ -1,4 +1,4 @@
 export class Declare {
     public static serverApiPath: string = "https://192.168.31.103:44399/api/";
-    public static serverPath: string = "https://localhost:5001/";
+    public static realtimeServer: string = "https://localhost:5001/";
 }
